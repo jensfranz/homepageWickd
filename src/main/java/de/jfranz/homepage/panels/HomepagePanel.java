19ddda97d0485986e0122927c92532211ffd700c
jens <jens@jens-X201EP>
2013-01-21 22:24:52 +0100

    added res

---
+++ src/main/java/de/jfranz/homepage/panels/HomepagePanel.java
@@ -0,0 +1,18 @@
+package de.jfranz.homepage.panels;
+
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+
+public abstract class HomepagePanel extends Panel {
+
+	public HomepagePanel(String id, IModel<?> model) {
+		super(id, model);
+		// TODO Auto-generated constructor stub
+	}
+
+	public HomepagePanel(String id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/de/jfranz/homepage/panels/MenuPanel.java
@@ -0,0 +1,17 @@
+package de.jfranz.homepage.panels;
+
+import org.apache.wicket.model.IModel;
+
+public class MenuPanel extends HomepagePanel {
+
+	public MenuPanel(String id, IModel<?> model) {
+		super(id, model);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MenuPanel(String id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+
+}
